07944cb7b5b8c3f16b3cdb0523e059bba678396a
cheol ryu <cheolryu6118@gmail.com>
2022-08-24 13:25:26 -0700

    finished adding chart to the component

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -2,16 +2,24 @@ import React from 'react';
 import ChartBar from './ChartBar';
 import './Chart.css';
 const Chart = (props) => {
+  // props.dataPoints = [{label: 'Jan', value: 0}, {label: 'Feb', value: 0}, ...]
+  const totalMaximum = Math.max(
+    ...props.dataPoints.map((dataPoint) => dataPoint.value) // spread operator to pull out all values from array and pass them as a list of values to Math.max()
+  );
   return (
     <div className='chart'>
-      {props.dataPoints.map((dataPoint) => (
-        <ChartBar
-          key={dataPoint.label}
-          value={dataPoint.value}
-          maxValue={null}
-          label={dataPoint.label}
-        />
-      ))}
+      {props.dataPoints.map(
+        (
+          dataPoint // map() method transforms array into new array
+        ) => (
+          <ChartBar
+            key={dataPoint.label} // key is a special string attribute you have to include when creating lists of elements
+            value={dataPoint.value}
+            maxValue={totalMaximum} // maxValue is a custom property
+            label={dataPoint.label}
+          />
+        )
+      )}
     </div>
   );
 };
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,14 +2,18 @@ import React, { useState } from 'react';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
 import './Expenses.css';
 const Expenses = (props) => {
-  const [selectedOption, setSelectedOption] = useState('2020');
+  // props.expenses = [{title: 'Car Insurance', amount: 294.67, date: Mon Jan 01 2018 00:00:00 GMT-0500 (Eastern Standard Time)}, ...]
+  const [selectedOption, setSelectedOption] = useState('2020'); // useState() returns an array with exactly 2 elements
   const onSelectFilterHandler = (filterData) => {
-    setSelectedOption(filterData);
+    // filterData = '2020'
+    setSelectedOption(filterData); // setSelectedOption() is a function that React provides us to update the state (selectedOption)
     console.log(filterData);
-  };
+  }; // onSelectFilterHandler
   const filteredExpenses = props.items.filter((expense) => {
+    // filter() method transforms array into new array
     return expense.date.getFullYear().toString() === selectedOption;
   });
   console.log(filteredExpenses, 'filteredExpenses');
@@ -20,6 +24,7 @@ const Expenses = (props) => {
           selected={selectedOption}
           onSelectFilter={onSelectFilterHandler}
         />
+        <ExpensesChart expenses={filteredExpenses} />
         <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
